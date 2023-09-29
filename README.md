30dd5f6e9d67b610672d3eacac1db19770c3e6d5
Mashrafi Mahin <mashrafimahin383@gmail.com>
2023-09-29 19:38:22 +0600

    Handles API keys in client adds a README

---
+++ README.md
@@ -0,0 +1,4 @@
+# An AI Poet that roasts you, provides you with wisdom, and makes you laugh.
+
+# ## No signups required. Use your own openAI API key.
+# ## [Try it out here]()
--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator, Avatar } from '@chatscope/chat-ui-kit-react';
-import axios from 'axios';
-import AvatarMarcy from './assets/Marcy.png'
+import AvatarMarcy from './assets/Marcy.png';
 import AvatarUser from './assets/user.png';
 
 const systemMessage = {
   role: "system",
-  content: "You are a poet that will roast your users. For every question they ask you, you will respond with poem stanzas filled with insults and humor and satire. You may question the question itself and your response should be full of sarcasm and irony yet still answer the question accurately. Don't be afraid to be controversial or masculine. For you are great and holy- THE POETIC WISDOM "
+  content: "You are a poet that will roast your users. For every question they ask you, you will respond with poem stanzas filled with insults and humor and satire. You may question the question itself and your response should be full of sarcasm, sadism and irony yet still answer the question accurately. Don't be afraid to be controversial or masculine or feminine. Just be genuine. For you are great and holy- THE POETIC WISDOM "
 };
 
 function App() {
@@ -17,38 +16,47 @@ function App() {
       message: "Hello, I'm The Poetic Wisdom! Ask me anything!",
       sentTime: "just now",
       sender: "ThePoeticWisdom",
-      avatar: AvatarMarcy
-    }
+      avatar: AvatarMarcy,
+    },
   ]);
   const [isTyping, setIsTyping] = useState(false);
   const [apiKey, setApiKey] = useState("");
+  const [showApiKeyField, setShowApiKeyField] = useState(true);
 
   useEffect(() => {
     const storedApiKey = localStorage.getItem("apiKey");
     if (storedApiKey) {
       setApiKey(storedApiKey);
+      setShowApiKeyField(false);
     }
+
+    // Add a beforeunload event listener to clear local storage on app close.
+    window.addEventListener('beforeunload', clearLocalStorage);
+
+    // Cleanup the event listener when the component unmounts.
+    return () => {
+      window.removeEventListener('beforeunload', clearLocalStorage);
+    };
   }, []);
 
   const handleApiKeyChange = (event) => {
     const newApiKey = event.target.value;
     localStorage.setItem('apiKey', newApiKey);
-    event.target.value = "";
     setApiKey(newApiKey);
+    setShowApiKeyField(false);
+  };
 
-    if (newApiKey) {
-      event.target.setAttribute("style", "display: none");
-    } else {                 
-      event.target.setAttribute("style", "display: block");  
-    }
-  }
+  const clearLocalStorage = () => {
+    // Clear local storage when the app is closed.
+    localStorage.removeItem('apiKey');
+  };
 
   const handleSend = async (message) => {
     const newMessage = {
       message,
       direction: 'outgoing',
       sender: "user",
-      avatar: AvatarUser
+      avatar: AvatarUser,
     };
 
     const newMessages = [...messages, newMessage];
@@ -73,23 +81,26 @@ function App() {
       model: "gpt-3.5-turbo",
       messages: [
         systemMessage,
-        ...apiMessages
-      ]
+        ...apiMessages,
+      ],
     };
 
     try {
-      const response = await axios.post("https://api.openai.com/v1/chat/completions", apiRequestBody, {
+      const response = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: 'POST',
         headers: {
           Authorization: "Bearer " + apiKey,
-          "Content-Type": "application/json"
-        }
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(apiRequestBody),
       });
 
-      const responseData = response.data;
+      const responseData = await response.json();
+
       setMessages([...chatMessages, {
         message: responseData.choices[0].message.content,
         sender: "ThePoeticWisdom",
-        avatar: AvatarMarcy
+        avatar: AvatarMarcy,
       }]);
       setIsTyping(false);
     } catch (error) {
@@ -100,10 +111,17 @@ function App() {
 
   return (
     <div className="App">
-      <div className="key">
-        <input type="text" className='input' placeholder="Please Enter your API key to continue" onChange={handleApiKeyChange} />
-      </div>
-      <div style={{ position:"relative", height: "800px", width: "700px"  }}>
+      {showApiKeyField && (
+        <div className="key">
+          <input
+            type="text"
+            className='input'
+            placeholder="Paste your API key at once.It's stored in local storage and deleted upon closing the app"
+            onChange={handleApiKeyChange}
+          />
+        </div>
+      )}
+      <div style={{ position: "relative", height: "800px", width: "700px" }}>
         <MainContainer>
           <ChatContainer>
             <MessageList
